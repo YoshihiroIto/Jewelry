9fc19c7f3ad66a6400d884dfefd23511c231f583
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-04-09 10:04:55 +0900

    Add HierarchicalMappingCollection

---
+++ Jewelry.Test/Collections/HierarchicalMappingCollectionTest.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections.ObjectModel;
+using Jewelry.Collections;
+using Xunit;
+
+namespace Jewelry.Test.Collections
+{
+    public class HierarchicalMappingCollectionTest
+    {
+        public class TreeNode
+        {
+            public int Param1 { get; set; }
+
+            public ObservableCollection<TreeNode> Children { get; set; } = new ObservableCollection<TreeNode>();
+        }
+
+        public class TreeNodeViewModel
+        {
+            public int Param1
+            {
+                get => _model.Param1;
+                set => _model.Param1 = value;
+            }
+            public bool IsSelected { get; set; }
+
+            public ObservableCollection<TreeNodeViewModel> Children { get; set; } = new ObservableCollection<TreeNodeViewModel>();
+
+            private readonly TreeNode _model;
+
+            public TreeNodeViewModel(TreeNode model)
+            {
+                _model = model;
+            }
+        }
+
+        [Fact]
+        public void InitialChildren()
+        {
+            var source = new ObservableCollection<TreeNode>
+            {
+                new TreeNode {Param1 = 123},
+                new TreeNode {Param1 = 456},
+                new TreeNode {Param1 = 789}
+            };
+
+            var target = new HierarchicalMappingCollection<TreeNode, TreeNodeViewModel>(
+                source, 
+                m => new TreeNodeViewModel(m),
+                s => s.Children,
+                t => t.Children);
+
+            Assert.Equal(3, source.Count);
+            Assert.Equal(3, target.Count);
+            Assert.Equal(123, target[0].Param1);
+            Assert.Equal(456, target[1].Param1);
+            Assert.Equal(789, target[2].Param1);
+        }
+
+        [Fact]
+        public void Add()
+        {
+            var source = new ObservableCollection<TreeNode>();
+
+            var target = new HierarchicalMappingCollection<TreeNode, TreeNodeViewModel>(
+                source, 
+                m => new TreeNodeViewModel(m),
+                s => s.Children,
+                t => t.Children);
+
+            Assert.Empty(source);
+            Assert.Empty(target);
+
+            source.Add(new TreeNode { Param1 = 123 });
+            source.Add(new TreeNode { Param1 = 456 });
+            source.Add(new TreeNode { Param1 = 789 });
+            
+            Assert.Equal(3, source.Count);
+            Assert.Equal(3, target.Count);
+            Assert.Equal(123, target[0].Param1);
+            Assert.Equal(456, target[1].Param1);
+            Assert.Equal(789, target[2].Param1);
+        }
+
+        [Fact]
+        public void Remove()
+        {
+            var source = new ObservableCollection<TreeNode>();
+
+            var target = new HierarchicalMappingCollection<TreeNode, TreeNodeViewModel>(
+                source, 
+                m => new TreeNodeViewModel(m),
+                s => s.Children,
+                t => t.Children);
+
+            Assert.Empty(source);
+            Assert.Empty(target);
+
+            source.Add(new TreeNode { Param1 = 123 });
+            source.Add(new TreeNode { Param1 = 456 });
+            source.Add(new TreeNode { Param1 = 789 });
+            
+            Assert.Equal(3, source.Count);
+            Assert.Equal(3, target.Count);
+            Assert.Equal(123, target[0].Param1);
+            Assert.Equal(456, target[1].Param1);
+            Assert.Equal(789, target[2].Param1);
+
+            source.RemoveAt(1);
+
+            Assert.Equal(2, source.Count);
+            Assert.Equal(2, target.Count);
+            Assert.Equal(123, target[0].Param1);
+            Assert.Equal(789, target[1].Param1);
+        }
+
+        [Fact]
+        public void ChildrenAdd()
+        {
+            var source = new ObservableCollection<TreeNode>();
+
+            var target = new HierarchicalMappingCollection<TreeNode, TreeNodeViewModel>(
+                source, 
+                m => new TreeNodeViewModel(m),
+                s => s.Children,
+                t => t.Children);
+
+            Assert.Empty(source);
+            Assert.Empty(target);
+
+            source.Add(new TreeNode { Param1 = 123 });
+            source.Add(new TreeNode { Param1 = 456 });
+            source.Add(new TreeNode { Param1 = 789 });
+
+            source[1].Children.Add(new TreeNode{Param1 = 111});
+
+            Assert.Equal(111, target[1].Children[0].Param1);
+        }
+
+        [Fact]
+        public void ChildrenRemove()
+        {
+            var source = new ObservableCollection<TreeNode>();
+
+            var target = new HierarchicalMappingCollection<TreeNode, TreeNodeViewModel>(
+                source, 
+                m => new TreeNodeViewModel(m),
+                s => s.Children,
+                t => t.Children);
+
+            Assert.Empty(source);
+            Assert.Empty(target);
+
+            source.Add(new TreeNode { Param1 = 123 });
+            source.Add(new TreeNode { Param1 = 456 });
+            source.Add(new TreeNode { Param1 = 789 });
+
+            source[1].Children.Add(new TreeNode{Param1 = 111});
+            Assert.Equal(111, target[1].Children[0].Param1);
+
+            source[1].Children.RemoveAt(0);
+            Assert.Empty(target[1].Children);
+        }
+
+        [Fact]
+        public void DeepChildrenAdd()
+        {
+            var source = new ObservableCollection<TreeNode>();
+
+            var target = new HierarchicalMappingCollection<TreeNode, TreeNodeViewModel>(
+                source, 
+                m => new TreeNodeViewModel(m),
+                s => s.Children,
+                t => t.Children);
+
+            Assert.Empty(source);
+            Assert.Empty(target);
+
+
+            var childNode1 = new TreeNode {Param1 = 111};
+            var childNode2 = new TreeNode {Param1 = 222};
+            var childNode3 = new TreeNode {Param1 = 333};
+
+            childNode1.Children.Add(childNode2);
+            childNode2.Children.Add(childNode3);
+
+            source.Add(childNode1);
+
+            Assert.Equal(111, target[0].Param1);
+            Assert.Equal(222, target[0].Children[0].Param1);
+            Assert.Equal(333, target[0].Children[0].Children[0].Param1);
+        }
+
+        [Fact]
+        public void ToHierarchicalMappingCollection()
+        {
+            var source = new ObservableCollection<TreeNode>
+            {
+                new TreeNode {Param1 = 123},
+                new TreeNode {Param1 = 456},
+                new TreeNode {Param1 = 789}
+            };
+
+            var target = source.ToHierarchicalMappingCollection<TreeNode, TreeNodeViewModel>(
+                m => new TreeNodeViewModel(m),
+                s => s.Children,
+                t => t.Children);
+
+            Assert.Equal(3, source.Count);
+            Assert.Equal(3, target.Count);
+            Assert.Equal(123, target[0].Param1);
+            Assert.Equal(456, target[1].Param1);
+            Assert.Equal(789, target[2].Param1);
+        }
+    }
+}
---
+++ Jewelry/Collections/HierarchicalMappingCollection.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Diagnostics;
+
+namespace Jewelry.Collections
+{
+    // -----------------------------------------------------------------------------------------------
+    public class HierarchicalMappingCollection<TSource, TTarget> : ObservableCollection<TTarget>, IDisposable
+    {
+        internal readonly Func<TSource, INotifyCollectionChanged> GetSourceChildren;
+        internal readonly Func<TTarget, Collection<TTarget>> GetTargetChildren;
+
+        private readonly INotifyCollectionChanged _source;
+        private readonly Func<TSource, TTarget> _converter;
+        private readonly bool _disposeElement;
+
+        private readonly Dictionary<TSource, (TTarget, SourceCollectionChanged)> _itemDict =
+            new Dictionary<TSource, (TTarget, SourceCollectionChanged)>();
+
+        public HierarchicalMappingCollection(
+            INotifyCollectionChanged source,
+            Func<TSource, TTarget> converter,
+            Func<TSource, INotifyCollectionChanged> getSourceChildren,
+            Func<TTarget, Collection<TTarget>> getTargetChildren,
+            bool disposeElement = true)
+        {
+            _source = source;
+            _converter = converter;
+            GetSourceChildren = getSourceChildren;
+            GetTargetChildren = getTargetChildren;
+            _disposeElement = disposeElement;
+
+            _source.CollectionChanged += SourceOnCollectionChanged;
+
+            foreach (var child in (IEnumerable<TSource>)_source)
+                AddHierarchy(this, child);
+        }
+
+        public void Dispose()
+        {
+            foreach (var child in (IEnumerable<TSource>)_source)
+                RemoveHierarchy(this, child);
+
+            _source.CollectionChanged -= SourceOnCollectionChanged;
+        }
+
+        private void SourceOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    foreach (TSource item in e.NewItems)
+                        AddHierarchy(this, item);
+
+                    break;
+
+                case NotifyCollectionChangedAction.Remove:
+                    foreach (TSource item in e.OldItems)
+                        RemoveHierarchy(this, item);
+
+                    break;
+
+                case NotifyCollectionChangedAction.Move:
+                case NotifyCollectionChangedAction.Replace:
+                case NotifyCollectionChangedAction.Reset:
+                    throw new NotImplementedException();
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        internal void AddHierarchy(Collection<TTarget> targets, TSource sourceItem)
+        {
+            var targetItem = _converter(sourceItem);
+            var scc = new SourceCollectionChanged(this, targetItem);
+
+            _itemDict.Add(sourceItem, (targetItem, scc));
+            targets.Add(targetItem);
+
+            GetSourceChildren(sourceItem).CollectionChanged += scc.OnCollectionChanged;
+
+            var targetChildren = GetTargetChildren(targetItem);
+
+            var children = GetSourceChildren(sourceItem);
+            if (children != null)
+                foreach (var itemChild in (IEnumerable<TSource>)children)
+                    AddHierarchy(targetChildren, itemChild);
+        }
+
+        internal void RemoveHierarchy(Collection<TTarget> targets, TSource sourceItem)
+        {
+            Debug.Assert(_itemDict.ContainsKey(sourceItem));
+            var (targetItem, sourceCollectionChanged) = _itemDict[sourceItem];
+
+            var targetChildren = GetTargetChildren(targetItem);
+
+            var children = GetSourceChildren(sourceItem);
+            if (children != null)
+                foreach (var itemChild in (IEnumerable<TSource>)children)
+                    RemoveHierarchy(targetChildren, itemChild);
+
+            _itemDict.Remove(sourceItem);
+            targets.Remove(targetItem);
+
+            if (_disposeElement)
+                if (targetItem is IDisposable disposable)
+                    disposable.Dispose();
+
+            GetSourceChildren(sourceItem).CollectionChanged -= sourceCollectionChanged.OnCollectionChanged;
+        }
+
+        private class SourceCollectionChanged
+        {
+            private readonly HierarchicalMappingCollection<TSource, TTarget> _parent;
+            private readonly TTarget _target;
+
+            public SourceCollectionChanged(HierarchicalMappingCollection<TSource, TTarget> parent, TTarget target)
+            {
+                _parent = parent;
+                _target = target;
+            }
+
+            public void OnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+            {
+                switch (e.Action)
+                {
+                    case NotifyCollectionChangedAction.Add:
+                        foreach (TSource item in e.NewItems)
+                            _parent.AddHierarchy(_parent.GetTargetChildren(_target), item);
+
+                        break;
+
+                    case NotifyCollectionChangedAction.Remove:
+                        foreach (TSource item in e.OldItems)
+                            _parent.RemoveHierarchy(_parent.GetTargetChildren(_target), item);
+
+                        break;
+
+                    case NotifyCollectionChangedAction.Move:
+                    case NotifyCollectionChangedAction.Replace:
+                    case NotifyCollectionChangedAction.Reset:
+                        throw new NotImplementedException();
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
+
+    public static class HierarchicalMappingCollectionExtensions
+    {
+        public static HierarchicalMappingCollection<TSource, TTarget>
+            ToHierarchicalMappingCollection<TSource, TTarget>(
+                this INotifyCollectionChanged self,
+                Func<TSource, TTarget> converter,
+                Func<TSource, INotifyCollectionChanged> getSourceChildren,
+                Func<TTarget, Collection<TTarget>> getTargetChildren,
+                bool disposeElement = true)
+            => new HierarchicalMappingCollection<TSource, TTarget>(
+                self, converter, getSourceChildren, getTargetChildren, disposeElement);
+    }
+}
