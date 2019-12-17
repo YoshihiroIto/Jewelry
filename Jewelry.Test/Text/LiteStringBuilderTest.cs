27ffde889fb358e9a25842c45bc560fce9aaa2b8
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-12-17 14:06:06 +0900

    Add LiteStringBuilderTest

---
+++ Jewelry.Test/Text/LiteStringBuilderTest.cs
@@ -0,0 +1,154 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Jewelry.Text;
+using Xunit;
+
+// ReSharper disable StringLiteralTypo
+
+namespace Jewelry.Test.Text
+{
+    [SuppressMessage("ReSharper", "PossiblyImpureMethodCallOnReadonlyVariable")]
+    public class LiteStringBuilderTest
+    {
+        [Fact]
+        public void Smoke()
+        {
+            using var lsb = new LiteStringBuilder();
+        }
+
+        [Fact]
+        public void Simple()
+        {
+            using var lsb = new LiteStringBuilder();
+
+            lsb.Append("aaa");
+            lsb.Append("bbb");
+
+            Assert.Equal("aaabbb", lsb.ToString());
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(100)]
+        public void StackSimple(int size)
+        {
+            using var lsb = new LiteStringBuilder(stackalloc char[size]);
+
+            lsb.Append("aaa");
+            lsb.Append("bbb");
+
+            Assert.Equal("aaabbb", lsb.ToString());
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(100)]
+        public void ArrayPoolSimple(int size)
+        {
+            using var lsb = new LiteStringBuilder(size);
+
+            lsb.Append("aaa");
+            lsb.Append("bbb");
+
+            Assert.Equal("aaabbb", lsb.ToString());
+        }
+
+        [Fact]
+        public void AppendIfNotNull()
+        {
+            using var lsb = new LiteStringBuilder();
+
+            lsb.AppendIfNotNull("aaa");
+            lsb.AppendIfNotNull(null);
+            lsb.AppendIfNotNull("bbb");
+
+            Assert.Equal("aaabbb", lsb.ToString());
+
+
+            var a = new StringBuilder();
+
+            a.AppendLine("aaa");
+        }
+
+        [Fact]
+        public void AppendLine()
+        {
+            using var lsb = new LiteStringBuilder();
+
+            lsb.AppendLine("aaa");
+            lsb.AppendLine("bbb");
+
+            Assert.Equal("aaa\r\nbbb\r\n", lsb.ToString());
+        }
+
+        [Fact]
+        public void AppendLineIfNotNull()
+        {
+            using var lsb = new LiteStringBuilder();
+
+            lsb.AppendLine("aaa");
+            lsb.AppendLineIfNotNull(null);
+            lsb.AppendLine("bbb");
+
+            Assert.Equal("aaa\r\n\r\nbbb\r\n", lsb.ToString());
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(100)]
+        public void AppendInt(int size)
+        {
+            using var lsb = new LiteStringBuilder(stackalloc char[size]);
+
+            lsb.Append(1);
+            lsb.Append(234);
+            lsb.Append(56);
+
+            Assert.Equal("123456", lsb.ToString());
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(100)]
+        public void AppendIntFormat(int size)
+        {
+            using var lsb = new LiteStringBuilder(stackalloc char[size]);
+
+            lsb.Append(1, "D4");
+
+            Assert.Equal("0001", lsb.ToString());
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(100)]
+        public void AppendDouble(int size)
+        {
+            using var lsb = new LiteStringBuilder(stackalloc char[size]);
+
+            lsb.Append(1.5);
+            lsb.Append(2);
+            lsb.Append(56);
+
+            Assert.Equal("1.5256", lsb.ToString());
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(100)]
+        public void AppendDoubleFormat(int size)
+        {
+            using var lsb = new LiteStringBuilder(stackalloc char[size]);
+
+            lsb.Append(1.5, "F3");
+
+            Assert.Equal("1.500", lsb.ToString());
+        }
+    }
+}
---
+++ Jewelry/Text/LiteStringBuilder.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Buffers;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Based code
+// https://github.com/dotnet/corefx/blob/release/3.1/src/Common/src/CoreLib/System/Text/ValueStringBuilder.cs を元にしました。
+
+namespace Jewelry.Text
+{
+    public ref struct LiteStringBuilder
+    {
+        #nullable enable
+
+        private char[]? _arrayToReturnToPool;
+        private Span<char> _chars;
+        private int _pos;
+
+        public LiteStringBuilder(Span<char> initialBuffer)
+        {
+            _arrayToReturnToPool = null;
+            _chars = initialBuffer;
+            _pos = 0;
+        }
+
+        public LiteStringBuilder(int initialCapacity)
+        {
+            _arrayToReturnToPool = ArrayPool<char>.Shared.Rent(initialCapacity);
+            _chars = _arrayToReturnToPool;
+            _pos = 0;
+        }
+
+        public int Length
+        {
+            get => _pos;
+            set
+            {
+                Debug.Assert(value >= 0);
+                Debug.Assert(value <= _chars.Length);
+                _pos = value;
+            }
+        }
+
+        public int Capacity => _chars.Length;
+
+        public void EnsureCapacity(int capacity)
+        {
+            if (capacity > _chars.Length)
+                Grow(capacity - _pos);
+        }
+
+        /// <summary>
+        /// Get a pinnable reference to the builder.
+        /// Does not ensure there is a null char after <see cref="Length"/>
+        /// This overload is pattern matched in the C# 7.3+ compiler so you can omit
+        /// the explicit method call, and write eg "fixed (char* c = builder)"
+        /// </summary>
+        public ref char GetPinnableReference()
+        {
+            return ref MemoryMarshal.GetReference(_chars);
+        }
+
+        /// <summary>
+        /// Get a pinnable reference to the builder.
+        /// </summary>
+        /// <param name="terminate">Ensures that the builder has a null char after <see cref="Length"/></param>
+        public ref char GetPinnableReference(bool terminate)
+        {
+            if (terminate)
+            {
+                EnsureCapacity(Length + 1);
+                _chars[Length] = '\0';
+            }
+            return ref MemoryMarshal.GetReference(_chars);
+        }
+
+        public ref char this[int index]
+        {
+            get
+            {
+                Debug.Assert(index < _pos);
+                return ref _chars[index];
+            }
+        }
+
+        public override string ToString()
+        {
+            var s = _chars.Slice(0, _pos).ToString();
+            Dispose();
+            return s;
+        }
+
+        /// <summary>Returns the underlying storage of the builder.</summary>
+        public Span<char> RawChars => _chars;
+
+        /// <summary>
+        /// Returns a span around the contents of the builder.
+        /// </summary>
+        /// <param name="terminate">Ensures that the builder has a null char after <see cref="Length"/></param>
+        public ReadOnlySpan<char> AsSpan(bool terminate)
+        {
+            if (terminate)
+            {
+                EnsureCapacity(Length + 1);
+                _chars[Length] = '\0';
+            }
+            return _chars.Slice(0, _pos);
+        }
+
+        public ReadOnlySpan<char> AsSpan() => _chars.Slice(0, _pos);
+        public ReadOnlySpan<char> AsSpan(int start) => _chars.Slice(start, _pos - start);
+        public ReadOnlySpan<char> AsSpan(int start, int length) => _chars.Slice(start, length);
+
+        public bool TryCopyTo(Span<char> destination, out int charsWritten)
+        {
+            if (_chars.Slice(0, _pos).TryCopyTo(destination))
+            {
+                charsWritten = _pos;
+                Dispose();
+                return true;
+            }
+            else
+            {
+                charsWritten = 0;
+                Dispose();
+                return false;
+            }
+        }
+
+        public void Insert(int index, char value, int count)
+        {
+            if (_pos > _chars.Length - count)
+            {
+                Grow(count);
+            }
+
+            int remaining = _pos - index;
+            _chars.Slice(index, remaining).CopyTo(_chars.Slice(index + count));
+            _chars.Slice(index, count).Fill(value);
+            _pos += count;
+        }
+
+        public void Insert(int index, string s)
+        {
+            int count = s.Length;
+
+            if (_pos > (_chars.Length - count))
+            {
+                Grow(count);
+            }
+
+            int remaining = _pos - index;
+            _chars.Slice(index, remaining).CopyTo(_chars.Slice(index + count));
+            s.AsSpan().CopyTo(_chars.Slice(index));
+            _pos += count;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Append(char c)
+        {
+            int pos = _pos;
+            if ((uint)pos < (uint)_chars.Length)
+            {
+                _chars[pos] = c;
+                _pos = pos + 1;
+            }
+            else
+            {
+                GrowAndAppend(c);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Append(string s)
+        {
+            int pos = _pos;
+            if (s.Length == 1 && (uint)pos < (uint)_chars.Length) // very common case, e.g. appending strings from NumberFormatInfo like separators, percent symbols, etc.
+            {
+                _chars[pos] = s[0];
+                _pos = pos + 1;
+            }
+            else
+            {
+                AppendSlow(s);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void AppendLine(string s)
+        {
+            Append(s);
+            AppendNewLine();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void AppendIfNotNull(string? s)
+        {
+            if (s == null)
+                return;
+
+            Append(s);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void AppendLineIfNotNull(string s)
+        {
+            if (s != null)
+                Append(s);
+
+            AppendNewLine();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void AppendNewLine()
+        {
+            Append('\r');
+            Append('\n');
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Append(short v, ReadOnlySpan<char> format = default)
+        {
+retry:
+            if (v.TryFormat(_chars.Slice(_pos), out var charsWritten, format) == false)
+            {
+                Grow(32);
+
+                goto retry;
+            }
+                
+            _pos += charsWritten;
+
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Append(int v, ReadOnlySpan<char> format = default)
+        {
+retry:
+            if (v.TryFormat(_chars.Slice(_pos), out var charsWritten, format) == false)
+            {
+                Grow(32);
+
+                goto retry;
+            }
+                
+            _pos += charsWritten;
+
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Append(float v, ReadOnlySpan<char> format = default)
+        {
+retry:
+            if (v.TryFormat(_chars.Slice(_pos), out var charsWritten, format) == false)
+            {
+                Grow(32);
+
+                goto retry;
+            }
+                
+            _pos += charsWritten;
+
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Append(double v, ReadOnlySpan<char> format = default)
+        {
+retry:
+            if (v.TryFormat(_chars.Slice(_pos), out var charsWritten, format) == false)
+            {
+                Grow(32);
+
+                goto retry;
+            }
+                
+            _pos += charsWritten;
+        }
+
+
+        public void Append(char c, int count)
+        {
+            if (_pos > _chars.Length - count)
+            {
+                Grow(count);
+            }
+
+            Span<char> dst = _chars.Slice(_pos, count);
+            for (int i = 0; i < dst.Length; i++)
+            {
+                dst[i] = c;
+            }
+            _pos += count;
+        }
+
+        public void Append(ReadOnlySpan<char> value)
+        {
+            int pos = _pos;
+            if (pos > _chars.Length - value.Length)
+            {
+                Grow(value.Length);
+            }
+
+            value.CopyTo(_chars.Slice(_pos));
+            _pos += value.Length;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<char> AppendSpan(int length)
+        {
+            int origPos = _pos;
+            if (origPos > _chars.Length - length)
+            {
+                Grow(length);
+            }
+
+            _pos = origPos + length;
+            return _chars.Slice(origPos, length);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void GrowAndAppend(char c)
+        {
+            Grow(1);
+            Append(c);
+        }
+
+        private void AppendSlow(string s)
+        {
+            int pos = _pos;
+            if (pos > _chars.Length - s.Length)
+            {
+                Grow(s.Length);
+            }
+
+            s.AsSpan().CopyTo(_chars.Slice(pos));
+            _pos += s.Length;
+        }
+
+        /// <summary>
+        /// Resize the internal buffer either by doubling current buffer size or
+        /// by adding <paramref name="additionalCapacityBeyondPos"/> to
+        /// <see cref="_pos"/> whichever is greater.
+        /// </summary>
+        /// <param name="additionalCapacityBeyondPos">
+        /// Number of chars requested beyond current position.
+        /// </param>
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void Grow(int additionalCapacityBeyondPos)
+        {
+            Debug.Assert(additionalCapacityBeyondPos > 0);
+            Debug.Assert(_pos > _chars.Length - additionalCapacityBeyondPos, "Grow called incorrectly, no resize is needed.");
+
+            char[] poolArray = ArrayPool<char>.Shared.Rent(Math.Max(_pos + additionalCapacityBeyondPos, _chars.Length * 2));
+
+            _chars.CopyTo(poolArray);
+
+            char[]? toReturn = _arrayToReturnToPool;
+            _chars = _arrayToReturnToPool = poolArray;
+            if (toReturn != null)
+            {
+                ArrayPool<char>.Shared.Return(toReturn);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Dispose()
+        {
+            char[]? toReturn = _arrayToReturnToPool;
+            this = default; // for safety, to avoid using pooled array if this instance is erroneously appended to again
+            if (toReturn != null)
+            {
+                ArrayPool<char>.Shared.Return(toReturn);
+            }
+        }
+    }
+}
