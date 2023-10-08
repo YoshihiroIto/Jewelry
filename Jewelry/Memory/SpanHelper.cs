63eb2bab8551d9ee2e26c51c468ac496afbe19fd
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2023-10-09 08:23:52 +0900

    Add SpanHelper.LowerBound<T0, T1>, SpanHelper.UpperBound<T0, T1>

--- Jewelry/Memory/SpanHelper.cs
+++ Jewelry/Memory/SpanHelper.cs
@@ -41,4 +41,44 @@ public static class SpanHelper
 
         return l;
     }
+    
+    public delegate int Comparison<in T0, in T1>(T0 x, T1 y);
+    
+    public static int LowerBound<T0, T1>(ReadOnlySpan<T0> a, T1 v, Comparison<T0, T1> cmp)
+    {
+        var l = 0;
+        var r = a.Length - 1;
+
+        while (l <= r)
+        {
+            var mid = l + (r - l) / 2;
+            var res = cmp(a[mid], v);
+
+            if (res == -1)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+
+        return l;
+    }
+
+    public static int UpperBound<T0, T1>(ReadOnlySpan<T0> a, T1 v, Comparison<T0, T1> cmp)
+    {
+        var l = 0;
+        var r = a.Length - 1;
+
+        while (l <= r)
+        {
+            var mid = l + (r - l) / 2;
+            var res = cmp(a[mid], v);
+
+            if (res <= 0)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+
+        return l;
+    }
 }
