1bf9ddb7985920bf5a4db3f735f2d4013a9c4996
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2023-12-02 20:36:22 +0900

    Add Sha1

---
+++ Jewelry.Test/Text/Sha1Tests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using Jewelry.Text;
+
+namespace Jewelry.Test.Text;
+
+public sealed class Sha1Tests
+{
+    [Fact]
+    public void Parse()
+    {
+        var sut = new Sha1("a78ca3a164cebb516a6fda68b655b7903a895209");
+
+        var result = sut.ToString();
+
+        Assert.Equal("a78ca3a164cebb516a6fda68b655b7903a895209", result);
+    }
+}
---
+++ Jewelry/Text/Sha1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Jewelry.Text;
+
+public readonly struct Sha1
+{
+    private readonly UInt128 _value0;
+    private readonly uint _value1;
+
+    public Sha1(string source)
+    {
+        var s = source.AsSpan();
+
+        _value0 = new UInt128(
+            ulong.Parse(s.Slice(0, 16), System.Globalization.NumberStyles.HexNumber),
+            ulong.Parse(s.Slice(16, 16), System.Globalization.NumberStyles.HexNumber));
+        _value1 = uint.Parse(s.Slice(32, 8), System.Globalization.NumberStyles.HexNumber);
+    }
+
+    public override string ToString()
+    {
+        return $"{_value0:x32}{_value1:x8}";
+    }
+}
