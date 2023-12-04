e4e1599db5ba07353e832625a1ad2742f7f7ad28
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2023-12-04 20:16:41 +0900

    Add CompactList

---
+++ Jewelry.Test/Collections/CompactListTests.cs
@@ -0,0 +1,101 @@
+﻿using Jewelry.Collections;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Jewelry.Test.Collections;
+
+public sealed class CompactListTests
+{
+    [Fact]
+    public void Single_value()
+    {
+        var sut = new CompactList<int> { 123 };
+
+        Assert.Equal(1, sut.Count);
+        Assert.Equal(123, sut[0]);
+        Assert.True(sut.SequenceEqual(new[] { 123 }));
+        Assert.Throws<IndexOutOfRangeException>(() => _ = sut[1]);
+    }
+
+    [Fact]
+    public void Multiple_values()
+    {
+        var sut = new CompactList<int> { 123, 456, 789 };
+
+        Assert.Equal(3, sut.Count);
+        Assert.Equal(123, sut[0]);
+        Assert.Equal(456, sut[1]);
+        Assert.Equal(789, sut[2]);
+        Assert.True(sut.SequenceEqual(new[] { 123, 456, 789 }));
+        Assert.Throws<IndexOutOfRangeException>(() => _ = sut[3]);
+    }
+    
+    [Fact]
+    public void Empty()
+    {
+        var sut = new CompactList<int>();
+
+        Assert.Equal(0, sut.Count);
+        Assert.True(sut.SequenceEqual(new int[] { }));
+        Assert.Throws<IndexOutOfRangeException>(() => _ = sut[0]);
+    }
+    
+    [Fact]
+    public void Add_single_value()
+    {
+        // ReSharper disable once UseObjectOrCollectionInitializer
+        var sut = new CompactList<int>();
+        
+        sut.Add(123);
+
+        Assert.Equal(1, sut.Count);
+        Assert.Equal(123, sut[0]);
+        Assert.True(sut.SequenceEqual(new[] { 123 }));
+        Assert.Throws<IndexOutOfRangeException>(() => _ = sut[1]);
+    }
+    
+    [Fact]
+    public void Add_multiple_value()
+    {
+        // ReSharper disable once UseObjectOrCollectionInitializer
+        var sut = new CompactList<int>();
+        
+        sut.Add(123);
+        sut.Add(456);
+        sut.Add(789);
+
+        Assert.Equal(3, sut.Count);
+        Assert.Equal(123, sut[0]);
+        Assert.Equal(456, sut[1]);
+        Assert.Equal(789, sut[2]);
+        Assert.True(sut.SequenceEqual(new[] { 123, 456, 789 }));
+        Assert.Throws<IndexOutOfRangeException>(() => _ = sut[3]);
+    }
+    
+    [Fact]
+    public void Clear()
+    {
+        var sut = new CompactList<int> { 123, 456, 789 };
+        
+        sut.Clear();
+
+        Assert.Equal(0, sut.Count);
+        Assert.True(sut.SequenceEqual(new int[] { }));
+        Assert.Throws<IndexOutOfRangeException>(() => _ = sut[0]);
+    }
+    
+    [Fact]
+    public void ToCompactList()
+    {
+        var sut = new[]{ 123, 456, 789 }.ToCompactList();
+
+        Assert.Equal(3, sut.Count);
+        Assert.Equal(123, sut[0]);
+        Assert.Equal(456, sut[1]);
+        Assert.Equal(789, sut[2]);
+        Assert.True(sut.SequenceEqual(new[] { 123, 456, 789 }));
+        Assert.Throws<IndexOutOfRangeException>(() => _ = sut[3]);
+    }
+    
+}
---
+++ Jewelry/Collections/CompactList.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Jewelry.Collections;
+
+// Implemented using this code as a reference
+// https://github.com/JetBrains/rd/blob/master/rd-net/Lifetimes/Collections/CompactList.cs
+
+public struct CompactList<T> : IEnumerable<T>
+{
+    public CompactListEnumerator<T> GetEnumerator() => new(_singleValue, _multipleValues);
+    IEnumerator<T> IEnumerable<T>.GetEnumerator() => GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public int Count => _multipleValues == IsSingle ? 1 : _multipleValues?.Count ?? 0;
+    
+    internal static readonly List<T?> IsSingle = new();
+    private T? _singleValue;
+    private List<T?>? _multipleValues;
+
+    public void Add(T item)
+    {
+        switch (Count)
+        {
+            case 0:
+                _singleValue = item;
+                _multipleValues = IsSingle;
+                break;
+
+            case 1:
+                _multipleValues = new List<T?> { _singleValue, item };
+                _singleValue = default;
+                break;
+
+            default:
+                _ = _multipleValues ?? throw new InvalidOperationException();
+                _multipleValues.Add(item);
+                break;
+        }
+    }
+
+    public void Clear()
+    {
+        _singleValue = default;
+        _multipleValues = default;
+    }
+
+    public T? this[int index]
+    {
+        get
+        {
+            if (_multipleValues == IsSingle)
+            {
+                if (index == 0)
+                    return _singleValue;
+
+                throw new IndexOutOfRangeException();
+            }
+
+            if (_multipleValues is null)
+                throw new IndexOutOfRangeException();
+
+            if(index >= Count)
+                throw new IndexOutOfRangeException();
+                
+            return _multipleValues[index];
+        }
+    }
+}
+
+public struct CompactListEnumerator<T> : IEnumerator<T?>
+{
+    public T? Current { get; private set; }
+    object? IEnumerator.Current => Current;
+    
+    private readonly T? _singleValue;
+    private readonly List<T?>? _multipleValues;
+    private int _index;
+
+    internal CompactListEnumerator(T? singleValue, List<T?>? multipleValues)
+    {
+        _singleValue = singleValue;
+        _multipleValues = multipleValues;
+        _index = -1;
+    }
+
+    public bool MoveNext()
+    {
+        if (_multipleValues is null)
+            return false;
+
+        ++_index;
+
+        if (_multipleValues == CompactList<T>.IsSingle)
+        {
+            if (_index == 0)
+            {
+                Current = _singleValue;
+                return true;
+            }
+        }
+        else
+        {
+            if (_index < _multipleValues.Count)
+            {
+                Current = _multipleValues[_index];
+                return true;
+            }
+        }
+
+        Current = default;
+        return false;
+    }
+
+    public void Reset()
+    {
+        _index = -1;
+        Current = default;
+    }
+
+    public void Dispose()
+    {
+    }
+}
+
+public static class CompactListExtensions
+{
+    public static CompactList<T> ToCompactList<T>(this IEnumerable<T> source)
+    {
+        var result = new CompactList<T>();
+
+        foreach (var item in source)
+            result.Add(item);
+
+        return result;
+    }
+}
