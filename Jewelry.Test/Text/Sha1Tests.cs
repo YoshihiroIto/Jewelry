b37a74323f12cfdc4456ba7c0fa5d58764a1a9d7
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2024-01-07 14:43:30 +0900

    Add Sha1.operator ==

--- Jewelry.Test/Text/Sha1Tests.cs
+++ Jewelry.Test/Text/Sha1Tests.cs
@@ -14,4 +14,26 @@ public sealed class Sha1Tests
 
         Assert.Equal("a78ca3a164cebb516a6fda68b655b7903a895209", result);
     }
+    
+    [Fact]
+    public void OperatorEquals()
+    {
+        var sha1 = new Sha1("a78ca3a164cebb516a6fda68b655b7903a895209");
+        var sha2 = new Sha1("a78ca3a164cebb516a6fda68b655b7903a895209");
+
+        var result = sha1 == sha2;
+
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public void OperatorNotEquals()
+    {
+        var sha1 = new Sha1("a78ca3a164cebb516a6fda68b655b7903a895209");
+        var sha2 = new Sha1("b78ca3a164cebb516a6fda68b655b7903a895209");
+
+        var result = sha1 != sha2;
+
+        Assert.True(result);
+    }
 }
--- Jewelry/Text/Sha1.cs
+++ Jewelry/Text/Sha1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace Jewelry.Text;
 
@@ -19,7 +20,7 @@ public readonly struct Sha1
 
     public override string ToString()
     {
-        return string.Create(40, this, (span, t) =>
+        return string.Create(40, this, static (span, t) =>
         {
             for (var i = 0; i != 32; i++)
             {
@@ -33,5 +34,35 @@ public readonly struct Sha1
                 span[32 + i] = c < 10 ? (char)('0' + c) : (char)('a' + c - 10);
             }
         });
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator ==(Sha1 left, Sha1 right)
+    {
+        return left._value0 == right._value0 && left._value1 == right._value1;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator !=(Sha1 left, Sha1 right)
+    {
+        return left._value0 != right._value0 || left._value1 != right._value1;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool Equals(Sha1 other)
+    {
+        return this == other;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override bool Equals(object? obj)
+    {
+        return obj is Sha1 other && Equals(other);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_value0, _value1);
     }
 }
