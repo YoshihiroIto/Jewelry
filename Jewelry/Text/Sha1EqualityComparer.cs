cbc7a96779902bdcb0973d2539c69d074eb35fcb
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2024-01-10 07:30:00 +0900

    Add Sha1EqualityComparer

---
+++ Jewelry/Text/Sha1EqualityComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Jewelry.Text;
+
+public sealed  class Sha1EqualityComparer : IEqualityComparer<Sha1>
+{
+    public static readonly Sha1EqualityComparer Shared = new();
+    
+    public bool Equals(Sha1 x, Sha1 y)
+    {
+        return x == y;
+    }
+
+    public int GetHashCode(Sha1 obj)
+    {
+        return obj.GetHashCode();
+    }
+}
