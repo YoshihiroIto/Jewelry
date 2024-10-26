0cdd36f868221973b4aca2aabb410008ee980205
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2024-10-26 13:34:44 +0900

    Add ObservableCollectionEx<T>.Add(T)

--- Jewelry/Collections/ObservableCollectionEx.cs
+++ Jewelry/Collections/ObservableCollectionEx.cs
@@ -33,7 +33,7 @@ public sealed class ObservableCollectionEx<T> : ObservableCollection<T>
         if (IsInChanging == false)
             OnCollectionChanged(ResetEventArgs);
     }
-    
+
     public void AddRange(ReadOnlySpan<T> items)
     {
         foreach (var item in items)
@@ -52,6 +52,14 @@ public sealed class ObservableCollectionEx<T> : ObservableCollection<T>
         }
         else
             base.Clear();
+    }
+
+    public new void Add(T item)
+    {
+        if (IsInChanging)
+            Items.Add(item);
+        else
+            base.Add(item);
     }
 
     public new bool Remove(T item)
