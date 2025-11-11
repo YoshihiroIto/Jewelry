32959bd52d2071cc66b1cc787ef05c45b3898459
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2025-11-12 07:38:24 +0900

    feat: Update target frameworks and improve clarity
    - Adds .NET 10.0 to the project's target frameworks.
    - Replaces lambda expressions with `CompareTo` method calls for more concise and readable comparisons in `SpanHelperTest`.
    - Increments package version and adds a new target framework.
    - Refactors binary search implementations to use strict inequality for correct boundary determination.

--- Jewelry.Test/Memory/SpanHelperTest.cs
+++ Jewelry.Test/Memory/SpanHelperTest.cs
@@ -11,78 +11,78 @@ public class SpanHelperTest
     public void LowerBound_Empty()
     {
         var a = Array.Empty<int>();
-        Assert.Equal(0, SpanHelper.LowerBound(a.AsSpan(), 0, (x, y) => x < y ? -1 : x > y ? 1 : 0));
+        Assert.Equal(0, SpanHelper.LowerBound(a.AsSpan(), 0, (x, y) => x.CompareTo(y)));
     }
 
     [Fact]
     public void LowerBound_OneElement_Found()
     {
         var a = new[] { 1 };
-        Assert.Equal(0, SpanHelper.LowerBound(a.AsSpan(), 1, (x, y) => x < y ? -1 : x > y ? 1 : 0));
+        Assert.Equal(0, SpanHelper.LowerBound(a.AsSpan(), 1, (x, y) => x.CompareTo(y)));
     }
 
     [Fact]
     public void LowerBound_OneElement_NotFound()
     {
         var a = new[] { 1 };
-        Assert.Equal(0, SpanHelper.LowerBound(a.AsSpan(), 0, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(1, SpanHelper.LowerBound(a.AsSpan(), 2, (x, y) => x < y ? -1 : x > y ? 1 : 0));
+        Assert.Equal(0, SpanHelper.LowerBound(a.AsSpan(), 0, (x, y) => x.CompareTo(y)));
+        Assert.Equal(1, SpanHelper.LowerBound(a.AsSpan(), 2, (x, y) => x.CompareTo(y)));
     }
 
     [Fact]
     public void LowerBound_MultipleElements()
     {
         var a = new[] { 1, 3, 5, 7, 9 };
-        Assert.Equal(0, SpanHelper.LowerBound(a.AsSpan(), 0, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(0, SpanHelper.LowerBound(a.AsSpan(), 1, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(1, SpanHelper.LowerBound(a.AsSpan(), 2, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(1, SpanHelper.LowerBound(a.AsSpan(), 3, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(2, SpanHelper.LowerBound(a.AsSpan(), 4, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(2, SpanHelper.LowerBound(a.AsSpan(), 5, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(3, SpanHelper.LowerBound(a.AsSpan(), 6, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(3, SpanHelper.LowerBound(a.AsSpan(), 7, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(4, SpanHelper.LowerBound(a.AsSpan(), 8, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(4, SpanHelper.LowerBound(a.AsSpan(), 9, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(5, SpanHelper.LowerBound(a.AsSpan(), 10, (x, y) => x < y ? -1 : x > y ? 1 : 0));
+        Assert.Equal(0, SpanHelper.LowerBound(a.AsSpan(), 0, (x, y) => x.CompareTo(y)));
+        Assert.Equal(0, SpanHelper.LowerBound(a.AsSpan(), 1, (x, y) => x.CompareTo(y)));
+        Assert.Equal(1, SpanHelper.LowerBound(a.AsSpan(), 2, (x, y) => x.CompareTo(y)));
+        Assert.Equal(1, SpanHelper.LowerBound(a.AsSpan(), 3, (x, y) => x.CompareTo(y)));
+        Assert.Equal(2, SpanHelper.LowerBound(a.AsSpan(), 4, (x, y) => x.CompareTo(y)));
+        Assert.Equal(2, SpanHelper.LowerBound(a.AsSpan(), 5, (x, y) => x.CompareTo(y)));
+        Assert.Equal(3, SpanHelper.LowerBound(a.AsSpan(), 6, (x, y) => x.CompareTo(y)));
+        Assert.Equal(3, SpanHelper.LowerBound(a.AsSpan(), 7, (x, y) => x.CompareTo(y)));
+        Assert.Equal(4, SpanHelper.LowerBound(a.AsSpan(), 8, (x, y) => x.CompareTo(y)));
+        Assert.Equal(4, SpanHelper.LowerBound(a.AsSpan(), 9, (x, y) => x.CompareTo(y)));
+        Assert.Equal(5, SpanHelper.LowerBound(a.AsSpan(), 10, (x, y) => x.CompareTo(y)));
     }
 
     [Fact]
     public void UpperBound_Empty()
     {
         var a = Array.Empty<int>();
-        Assert.Equal(0, SpanHelper.UpperBound(a.AsSpan(), 0, (x, y) => x < y ? -1 : x > y ? 1 : 0));
+        Assert.Equal(0, SpanHelper.UpperBound(a.AsSpan(), 0, (x, y) => x.CompareTo(y)));
     }
 
     [Fact]
     public void UpperBound_OneElement_Found()
     {
         var a = new[] { 1 };
-        Assert.Equal(1, SpanHelper.UpperBound(a.AsSpan(), 1, (x, y) => x < y ? -1 : x > y ? 1 : 0));
+        Assert.Equal(1, SpanHelper.UpperBound(a.AsSpan(), 1, (x, y) => x.CompareTo(y)));
     }
 
     [Fact]
     public void UpperBound_OneElement_NotFound()
     {
         var a = new[] { 1 };
-        Assert.Equal(0, SpanHelper.UpperBound(a.AsSpan(), 0, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(1, SpanHelper.UpperBound(a.AsSpan(), 2, (x, y) => x < y ? -1 : x > y ? 1 : 0));
+        Assert.Equal(0, SpanHelper.UpperBound(a.AsSpan(), 0, (x, y) => x.CompareTo(y)));
+        Assert.Equal(1, SpanHelper.UpperBound(a.AsSpan(), 2, (x, y) => x.CompareTo(y)));
     }
 
     [Fact]
     public void UpperBound_MultipleElements()
     {
         var a = new[] { 1, 3, 5, 7, 9 };
-        Assert.Equal(0, SpanHelper.UpperBound(a.AsSpan(), 0, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(1, SpanHelper.UpperBound(a.AsSpan(), 1, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(1, SpanHelper.UpperBound(a.AsSpan(), 2, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(2, SpanHelper.UpperBound(a.AsSpan(), 3, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(2, SpanHelper.UpperBound(a.AsSpan(), 4, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(3, SpanHelper.UpperBound(a.AsSpan(), 5, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(3, SpanHelper.UpperBound(a.AsSpan(), 6, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(4, SpanHelper.UpperBound(a.AsSpan(), 7, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(4, SpanHelper.UpperBound(a.AsSpan(), 8, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(5, SpanHelper.UpperBound(a.AsSpan(), 9, (x, y) => x < y ? -1 : x > y ? 1 : 0));
-        Assert.Equal(5, SpanHelper.UpperBound(a.AsSpan(), 10, (x, y) => x < y ? -1 : x > y ? 1 : 0));
+        Assert.Equal(0, SpanHelper.UpperBound(a.AsSpan(), 0, (x, y) => x.CompareTo(y)));
+        Assert.Equal(1, SpanHelper.UpperBound(a.AsSpan(), 1, (x, y) => x.CompareTo(y)));
+        Assert.Equal(1, SpanHelper.UpperBound(a.AsSpan(), 2, (x, y) => x.CompareTo(y)));
+        Assert.Equal(2, SpanHelper.UpperBound(a.AsSpan(), 3, (x, y) => x.CompareTo(y)));
+        Assert.Equal(2, SpanHelper.UpperBound(a.AsSpan(), 4, (x, y) => x.CompareTo(y)));
+        Assert.Equal(3, SpanHelper.UpperBound(a.AsSpan(), 5, (x, y) => x.CompareTo(y)));
+        Assert.Equal(3, SpanHelper.UpperBound(a.AsSpan(), 6, (x, y) => x.CompareTo(y)));
+        Assert.Equal(4, SpanHelper.UpperBound(a.AsSpan(), 7, (x, y) => x.CompareTo(y)));
+        Assert.Equal(4, SpanHelper.UpperBound(a.AsSpan(), 8, (x, y) => x.CompareTo(y)));
+        Assert.Equal(5, SpanHelper.UpperBound(a.AsSpan(), 9, (x, y) => x.CompareTo(y)));
+        Assert.Equal(5, SpanHelper.UpperBound(a.AsSpan(), 10, (x, y) => x.CompareTo(y)));
     }
 
     [Fact]
@@ -135,11 +135,7 @@ public class SpanHelperTest
 
         static int CompareStringChar(string x, char y)
         {
-            if (x[0] < y)
-                return -1;
-            if (x[0] == y)
-                return 0;
-            return 1;
+            return x[0].CompareTo(y);
         }
     }
 }
